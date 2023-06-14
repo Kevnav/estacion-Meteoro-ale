eca95887171e1b83fbad2dc3cf886ff4001f880a
Kevin Navarro <L191061105@mulege.tecnm.mx>
2023-06-13 20:10:11 -0700

    pie

--- myapp/app/Views/Templates/pie.php
+++ myapp/app/Views/Templates/pie.php
@@ -1,5 +1,5 @@
 footer>
     <div class="container">
-        <p class="text-muted text-end">Desarrollado por Alescarlett20 y Kevnav</p>
+        <p class="text-muted text-end">Desarrollado por Alescarlett20 y </p>
     </div>
 </footer>
